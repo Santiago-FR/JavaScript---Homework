c08d295f3472a34f596500ab70dfe6fd009a1acb
Santiago-FR <santiago_ricof@hotmail.com>
2019-05-22 01:18:28 -0400

    table part ready

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Reference to the table body
+var tbody = d3.select("tbody");
+
+// Arrow function
+// Create rows
+data.forEach((tableData) => {
+  var row = tbody.append("tr");
+// From each object append data ato each cell populating all rows
+  Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+// console.log(value)
+  });
+});
+
+// Filter button logic
+
+
+
+
+
