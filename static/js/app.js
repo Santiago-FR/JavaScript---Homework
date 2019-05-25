493b7875b1f8cc386f9effcf92597d1484bca4f5
Santiago-FR <santiago_ricof@hotmail.com>
2019-05-25 12:02:24 -0400

    java scrip complete level 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,11 @@
 // from data.js
 var tableData = data;
 console.log(tableData);
-
-// Reference to the table body
 var tbody = d3.select("tbody");
-
-// Arrow function
-// Create rows
+function buildTable(data){
+// Reference to the table body
+tbody.html("");
+// Create rows with Arrow function
 data.forEach((tableData) => {
   var row = tbody.append("tr");
 // From each object append data ato each cell populating all rows
@@ -16,10 +15,23 @@ data.forEach((tableData) => {
 // console.log(value)
   });
 });
+};
+buildTable(tableData);
 
 // Filter button logic
 
+var submit = d3.select("#filter-btn");
 
+function FilterData(){
+  d3.event.preventDefault(); 
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue)
+  var filteredData = data.filter(ufoS => ufoS.datetime === inputValue);
+  console.log(filteredData);
+  buildTable(filteredData);
+};
 
+d3.select("#filter-btn").on("click",FilterData);
 
 
